4581c44a0858242385cd1cdb198e9927e842c984
Blintastic <106162184+Blintastic@users.noreply.github.com>
2023-01-31 17:15:11 +0100

    .

--- AvA2/Assets/Scriot/TreeInstation/ForestInstantiation.cs
+++ AvA2/Assets/Scriot/TreeInstation/ForestInstantiation.cs
@@ -9,8 +9,24 @@ public class ForestInstantiation : MonoBehaviour
     public Vector3 scaleVariance;
     public Vector3 rotationVariance;
 
-    void Start()
+    private GameObject[] instantiatedTrees;
+
+    private void OnValidate()
     {
+        if (maxTrees < 0)
+        {
+            maxTrees = 0;
+        }
+
+        if (instantiatedTrees != null)
+        {
+            foreach (GameObject tree in instantiatedTrees)
+            {
+                Destroy(tree);
+            }
+        }
+
+        instantiatedTrees = new GameObject[maxTrees];
         for (int i = 0; i < maxTrees; i++)
         {
             Vector3 randomPos = new Vector3(Random.Range(terrain.transform.position.x, terrain.transform.position.x + terrain.terrainData.size.x), 0, Random.Range(terrain.transform.position.z, terrain.transform.position.z + terrain.terrainData.size.z));
@@ -20,6 +36,7 @@ public class ForestInstantiation : MonoBehaviour
             GameObject tree = Instantiate(prefab, randomPos, Quaternion.Euler(0, Random.Range(0, 360), 0));
             tree.transform.localScale += new Vector3(Random.Range(-scaleVariance.x, scaleVariance.x), Random.Range(-scaleVariance.y, scaleVariance.y), Random.Range(-scaleVariance.z, scaleVariance.z));
             tree.transform.Rotate(Random.Range(-rotationVariance.x, rotationVariance.x), Random.Range(-rotationVariance.y, rotationVariance.y), Random.Range(-rotationVariance.z, rotationVariance.z));
+            instantiatedTrees[i] = tree;
         }
     }
 }
