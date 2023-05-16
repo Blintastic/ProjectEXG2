d33dc849f3821cf4b9f4be15bd74042f6179e50a
SirMyrkus <nam.nguyen04@htl-salzburg.ac.at>
2023-05-16 15:59:45 +0200

    .

--- AvA2/Assets/MyGame/Scripts/Markus/lightManager.cs
+++ AvA2/Assets/MyGame/Scripts/Markus/lightManager.cs
@@ -1,8 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using ArtDotNet;
 
 public class lightManager : MonoBehaviour
 {
+    private ArtNetClient artClient;
 
+    [SerializeField]
+    private Light[] lights;
 }
