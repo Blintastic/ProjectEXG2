beeccca8fb2639049420d2c41cb4877cde234276
Blintastic <106162184+Blintastic@users.noreply.github.com>
2023-06-06 17:12:37 +0200

    .

---
+++ AvA2/Assets/MyGame/Scripts/Planet 3/Portal.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Portal : MonoBehaviour
+{
+
+    public static int keys = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        gameObject.GetComponent<MeshCollider>().enabled = false;
+        gameObject.GetComponent<MeshRenderer>().enabled = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(keys >= 3)
+        {
+            gameObject.GetComponent<MeshCollider>().enabled = true;
+            gameObject.GetComponent<MeshRenderer>().enabled = true;
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        SceneManager.LoadScene(4);
+    }
+}
